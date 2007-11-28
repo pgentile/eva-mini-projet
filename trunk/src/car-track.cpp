29df04a58ac247b30683a890fff0a20e8c2e48c2
frederic.le.roy29 <frederic.le.roy29@1a28957e-ba3e-0410-aa6f-399959d89a10>
2007-11-28 10:48:03 +0000

    Circuit en huit

--- trunk/src/car-track.cpp
+++ trunk/src/car-track.cpp
@@ -17,15 +17,50 @@ void CarTrack::initTrack()
 {
   //circular track
   //radius=500
-  double radius=200;
+  double radius=50;
   double xoffset=100;
   double yoffset=100;
-  for (unsigned int i=1;i<360;i+=10)
+  int i;
+  /*for (unsigned int i=1;i<360;i+=10)
     {
       double x=(RANDOM_NUMBER(0.0,10.0)+radius)*cos(i*M_PI/180)+xoffset;
       double y=(RANDOM_NUMBER(0.0,10.0)+radius)*sin(i*M_PI/180)+yoffset;
       std::cout<<"x= "<<x<<" ; y= "<<y<<std::endl;
       _trackPoints.push_back(Vector3D(x,y,RANDOM_NUMBER(0.0,20.0)));
+    }*/
+
+    for (i=120;i<360;i+=10)
+    {
+      double x=radius*cos(i*M_PI/180)+xoffset;
+      double y=radius*sin(i*M_PI/180)+yoffset;
+      std::cout<<"x= "<<x<<" ; y= "<<y<<std::endl;
+      _trackPoints.push_back(Vector3D(x,y,0));
+    }
+	
+    for (i=1;i<60;i+=10)
+    {
+      double x=radius*cos(i*M_PI/180)+xoffset;
+      double y=radius*sin(i*M_PI/180)+yoffset;
+      std::cout<<"x= "<<x<<" ; y= "<<y<<std::endl;
+      _trackPoints.push_back(Vector3D(x,y,0));
+    }
+
+    xoffset=100;
+    yoffset=200;
+    for (i=238;i>0;i-=10)
+    {
+      double x=radius*cos(i*M_PI/180)+xoffset;
+      double y=radius*sin(i*M_PI/180)+yoffset;
+      std::cout<<"x= "<<x<<" ; y= "<<y<<std::endl;
+      _trackPoints.push_back(Vector3D(x,y,0));
+    }
+
+    for (i=360;i>300;i-=10)
+    {
+      double x=radius*cos(i*M_PI/180)+xoffset;
+      double y=radius*sin(i*M_PI/180)+yoffset;
+      std::cout<<"x= "<<x<<" ; y= "<<y<<std::endl;
+      _trackPoints.push_back(Vector3D(x,y,0));
     }
 }
 
