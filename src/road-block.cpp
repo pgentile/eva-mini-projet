1b6de9dbc5944b00da36f6fdf0802318fef9013c
antoine.nicolas <antoine.nicolas@1a28957e-ba3e-0410-aa6f-399959d89a10>
2007-11-28 12:49:40 +0000

    Road Block continued

--- src/road-block.cpp
+++ src/road-block.cpp
@@ -6,7 +6,6 @@
 RoadBlock::RoadBlock(double width, Vector3D startPoint, Vector3D endPoint)
 :_width(width),_startPoint(startPoint),_endPoint(endPoint)
 {
-  
 }
 
 RoadBlock::~RoadBlock()
@@ -14,5 +13,10 @@ RoadBlock::~RoadBlock()
 
 void RoadBlock::render()
 {
-
+    // Renders a Road Block between StartPoint(xs,ys,zs) and EndPoint(xe,ye,ze)
+    glBegin(GL_QUADS);
+        // TODO
+    glEnd();     
 }
+
+
