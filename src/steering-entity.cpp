92f09f22f4152767893b029c0457f12998bb2f17
antoine.nicolas <antoine.nicolas@1a28957e-ba3e-0410-aa6f-399959d89a10>
2007-11-28 11:59:56 +0000

    Refactoring

--- src/steering-entity.cpp
+++ src/steering-entity.cpp
@@ -6,12 +6,12 @@ Shape* SteeringEntity::_steeringShape=NULL;
 
 SteeringEntity::SteeringEntity(SteeringSystem* system)
 {
-  _maxVelocity=RANDOM_NUMBER(1.0,100.0);
-  _maxAcceleration=RANDOM_NUMBER(1.0,100.0);
-  _mass=RANDOM_NUMBER(1.0,100.0);
-  _system=system;
-  _id=system->addEntity(this);
-  initShape();
+    _maxVelocity=RANDOM_NUMBER(1.0,100.0);
+    _maxAcceleration=RANDOM_NUMBER(1.0,100.0);
+    _mass=RANDOM_NUMBER(1.0,100.0);
+    _system=system;
+    _id=system->addEntity(this);
+    initShape();
 }
 
 SteeringEntity::~SteeringEntity()
@@ -19,32 +19,35 @@ SteeringEntity::~SteeringEntity()
 
 void SteeringEntity::update(double dt)
 {
-  Vector3D acceleration;
-  Vector3D oldVelocity=_velocity;
-  for (unsigned int i=0;i<_behaviors.size();i++)
+    Vector3D acceleration;
+    Vector3D oldVelocity=_velocity;
+    
+    for (unsigned int i=0;i<_behaviors.size();i++)
     {
-      acceleration=acceleration+
+        acceleration=acceleration+
         _behaviors[i]->getWeight()*
         _behaviors[i]->compute(this);
     }
-  acceleration=acceleration/_mass;
-  acceleration.truncate(_maxAcceleration);
-  _velocity=_velocity+acceleration*dt;
-  _velocity.truncate(_maxVelocity);
-  //update position & orientation
-  Vector3D position=getTransform()->getPosition();
-  setForward(_velocity);
-  getTransform()->setPosition(position+_velocity*dt);
+    
+    acceleration=acceleration/_mass;
+    acceleration.truncate(_maxAcceleration);
+    _velocity=_velocity+acceleration*dt;
+    _velocity.truncate(_maxVelocity);
+    
+    //update position & orientation
+    Vector3D position=getTransform()->getPosition();
+    setForward(_velocity);
+    getTransform()->setPosition(position+_velocity*dt);
 }
 
 void SteeringEntity::initShape()
 {
-  _steeringShape=new Shape();
-  ShapePart* part=new Axis();
-  part->getTransform()->setScale(5,5,5);
-  _steeringShape->addPart(part);
-  part=new Sphere();
-  _steeringShape->addPart(part);
-  setShape(_steeringShape);
+    _steeringShape=new Shape();
+    ShapePart* part=new Axis();
+    part->getTransform()->setScale(5,5,5);
+    _steeringShape->addPart(part);
+    part=new Sphere();
+    _steeringShape->addPart(part);
+    setShape(_steeringShape);
 }
 
