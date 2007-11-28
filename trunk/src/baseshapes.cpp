a0bb78203f1db7ebc4c6f9b7f8784d99a0c0c7c7
antoine.nicolas <antoine.nicolas@1a28957e-ba3e-0410-aa6f-399959d89a10>
2007-11-28 12:30:45 +0000

    Refactoring

--- trunk/src/baseshapes.cpp
+++ trunk/src/baseshapes.cpp
@@ -66,27 +66,26 @@ Axis::~Axis()
 
 void Axis::render()
 {
-  /* 
-     Affichage des axes de l'environnement
-  */
-  glDisable(GL_LIGHTING);
-  glBegin(GL_LINES);
-  /* X */
-  glColor3f(1.0,0,0);
-  glVertex3f(0,0,0);
-  glVertex3f(1,0,0);
-  /* Y */
-  glColor3f(0,1,0);
-  glVertex3f(0,0,0);
-  glVertex3f(0,1,0);
-  /* Z */
-  glColor3f(0,0,1);
-  glVertex3f(0,0,0);
-  glVertex3f(0,0,1);
-  glEnd();
-
-  glEnable(GL_LIGHTING);
-  
+    /* 
+       Affichage des axes de l'environnement
+    */
+    glDisable(GL_LIGHTING);
+    glBegin(GL_LINES);
+    /* X */
+    glColor3f(1.0,0,0);
+    glVertex3f(0,0,0);
+    glVertex3f(1,0,0);
+    /* Y */
+    glColor3f(0,1,0);
+    glVertex3f(0,0,0);
+    glVertex3f(0,1,0);
+    /* Z */
+    glColor3f(0,0,1);
+    glVertex3f(0,0,0);
+    glVertex3f(0,0,1);
+    glEnd();
+    
+    glEnable(GL_LIGHTING);
 }
 
 //-------------------------
@@ -104,20 +103,18 @@ Quad::~Quad()
 
 void Quad::render()
 {
-  //render a horizontal quad centered in 0 in local coordinates
-  
-  glBegin(GL_QUADS);
-  glNormal3f(0.0,0.0,1.0);
-  glTexCoord2f(1,1);
-  glVertex3f(_width/2,_height/2,0);
-  glTexCoord2f(0,1);
-  glVertex3f(-_width/2,_height/2,0);
-  glTexCoord2f(0,0);
-  glVertex3f(-_width/2,-_height/2,0);
-  glTexCoord2f(1,0);
-  glVertex3f(_width/2,-_height/2,0);
-  glEnd();
-
+    //render a horizontal quad centered in 0 in local coordinates
+    glBegin(GL_QUADS);
+    glNormal3f(0.0,0.0,1.0);
+    glTexCoord2f(1,1);
+    glVertex3f(_width/2,_height/2,0);
+    glTexCoord2f(0,1);
+    glVertex3f(-_width/2,_height/2,0);
+    glTexCoord2f(0,0);
+    glVertex3f(-_width/2,-_height/2,0);
+    glTexCoord2f(1,0);
+    glVertex3f(_width/2,-_height/2,0);
+    glEnd();
 }
 
 //-----------------------------------------------
@@ -165,35 +162,36 @@ Mesh::~Mesh()
 
 void Mesh::render()
 {
-  glDisable(GL_CULL_FACE);
-  glBegin(GL_TRIANGLES);
-  for (unsigned int i=0;i<_indices.size();i+=3)
+    glDisable(GL_CULL_FACE);
+    glBegin(GL_TRIANGLES);
+    
+    for (unsigned int i=0;i<_indices.size();i+=3)
     {
-      Vector3D vertex0=_vertices[_indices[i]];
-      Vector3D vertex1=_vertices[_indices[i+1]];
-      Vector3D vertex2=_vertices[_indices[i+2]];
-      Vector3D normal0=_normals[_indices[i]];
-      Vector3D normal1=_normals[_indices[i+1]];
-      Vector3D normal2=_normals[_indices[i+2]];
-      
-      //    glColor3f(1,0,0);
-      glNormal3f(normal0.getX(),normal0.getY(),normal0.getZ());
-      glTexCoord2f(_texCoords[_indices[i]].first,_texCoords[_indices[i]].second);
-      glVertex3f(vertex0.getX(),vertex0.getY(),vertex0.getZ());
-      
-      //     glColor3f(0,1,0);
-      glNormal3f(normal1.getX(),normal1.getY(),normal1.getZ());
-      glTexCoord2f(_texCoords[_indices[i+1]].first,_texCoords[_indices[i+1]].second);
-      glVertex3f(vertex1.getX(),vertex1.getY(),vertex1.getZ());
-      
-      //     glColor3f(0,0,1);
-      glNormal3f(normal2.getX(),normal2.getY(),normal2.getZ());
-      glTexCoord2f(_texCoords[_indices[i+2]].first,_texCoords[_indices[i+2]].second);
-      glVertex3f(vertex2.getX(),vertex2.getY(),vertex2.getZ());
-
+        Vector3D vertex0=_vertices[_indices[i]];
+        Vector3D vertex1=_vertices[_indices[i+1]];
+        Vector3D vertex2=_vertices[_indices[i+2]];
+        Vector3D normal0=_normals[_indices[i]];
+        Vector3D normal1=_normals[_indices[i+1]];
+        Vector3D normal2=_normals[_indices[i+2]];
+        
+        //    glColor3f(1,0,0);
+        glNormal3f(normal0.getX(),normal0.getY(),normal0.getZ());
+        glTexCoord2f(_texCoords[_indices[i]].first,_texCoords[_indices[i]].second);
+        glVertex3f(vertex0.getX(),vertex0.getY(),vertex0.getZ());
+        
+        //     glColor3f(0,1,0);
+        glNormal3f(normal1.getX(),normal1.getY(),normal1.getZ());
+        glTexCoord2f(_texCoords[_indices[i+1]].first,_texCoords[_indices[i+1]].second);
+        glVertex3f(vertex1.getX(),vertex1.getY(),vertex1.getZ());
+        
+        //     glColor3f(0,0,1);
+        glNormal3f(normal2.getX(),normal2.getY(),normal2.getZ());
+        glTexCoord2f(_texCoords[_indices[i+2]].first,_texCoords[_indices[i+2]].second);
+        glVertex3f(vertex2.getX(),vertex2.getY(),vertex2.getZ());
     }
-   glEnd();
-  glEnable(GL_CULL_FACE);
+    
+    glEnd();
+    glEnable(GL_CULL_FACE);
 }
 
 //-----------------------------------------------
@@ -235,171 +233,171 @@ Grid::~Grid()
 
 AntShape::AntShape()
 {
-  //Corps
-  ShapePart* part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.2,0.2,0.3);
-  part1->getMaterial()->setShininess(0);
-  part1->getTransform()->setScale(2.0,2.0,2.0);
-  part1->getTransform()->setPosition(-2.0,0.0,2.0);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.2,0.2,0.3);
-  part1->getMaterial()->setShininess(0);
-  part1->getTransform()->setScale(2.0,2.0,2.0);
-  part1->getTransform()->setPosition(0.0,0.0,2.0);
-  addPart(part1);
-  //Tete
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.2,0.2,0.3);
-  part1->getMaterial()->setShininess(0);
-  part1->getTransform()->setPosition(2.0,0.0,2.0);
-  addPart(part1);
-  //Pate G
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(75,0,0);
-  part1->getTransform()->setPosition(0.0,4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(0.0,4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(45,0,0);
-  part1->getTransform()->setPosition(0.0,6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-  //Pate G
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(75,0,0);
-  part1->getTransform()->setPosition(-1.0,4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(-1.0,4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(45,0,0);
-  part1->getTransform()->setPosition(-1.0,6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-  //Pate G
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(75,0,0);
-  part1->getTransform()->setPosition(1.0,4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(1.0,4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(45,0,0);
-  part1->getTransform()->setPosition(1.0,6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  //Pate D
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-75,0,0);
-  part1->getTransform()->setPosition(0.0,-4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(0.0,-4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-45,0,0);
-  part1->getTransform()->setPosition(0.0,-6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-  //Pate D
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-75,0,0);
-  part1->getTransform()->setPosition(1.0,-4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(1.0,-4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-45,0,0);
-  part1->getTransform()->setPosition(1.0,-6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-  //Pate D
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-75,0,0);
-  part1->getTransform()->setPosition(-1.0,-4.0,2.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(-1.0,-4.0,2.1);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(-45,0,0);
-  part1->getTransform()->setPosition(-1.0,-6.0,0.0);
-  part1->getTransform()->setScale(0.2,0.2,3);
-  addPart(part1);
-  //Antennes
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(0,60,30);
-  part1->getTransform()->setPosition(2.5,0.3,2.0);
-  part1->getTransform()->setScale(0.1,0.1,3);
-  addPart(part1);
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(5.0,0.3,3.5);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
-
-  part1=new Cylinder();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setOrientation(0,60,-30);
-  part1->getTransform()->setPosition(2.5,-0.3,2.0);
-  part1->getTransform()->setScale(0.1,0.1,3);
-  addPart(part1);
-  part1=new Sphere();
-  part1->getMaterial()->setAmbient(0.0,0.0,0.0);
-  part1->getTransform()->setPosition(5.0,-0.3,3.5);
-  part1->getTransform()->setScale(0.3,0.3,0.3);
-  addPart(part1);
+    //Corps
+    ShapePart* part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.2,0.2,0.3);
+    part1->getMaterial()->setShininess(0);
+    part1->getTransform()->setScale(2.0,2.0,2.0);
+    part1->getTransform()->setPosition(-2.0,0.0,2.0);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.2,0.2,0.3);
+    part1->getMaterial()->setShininess(0);
+    part1->getTransform()->setScale(2.0,2.0,2.0);
+    part1->getTransform()->setPosition(0.0,0.0,2.0);
+    addPart(part1);
+    //Tete
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.2,0.2,0.3);
+    part1->getMaterial()->setShininess(0);
+    part1->getTransform()->setPosition(2.0,0.0,2.0);
+    addPart(part1);
+    //Pate G
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(75,0,0);
+    part1->getTransform()->setPosition(0.0,4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(0.0,4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(45,0,0);
+    part1->getTransform()->setPosition(0.0,6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    //Pate G
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(75,0,0);
+    part1->getTransform()->setPosition(-1.0,4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(-1.0,4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(45,0,0);
+    part1->getTransform()->setPosition(-1.0,6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    //Pate G
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(75,0,0);
+    part1->getTransform()->setPosition(1.0,4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(1.0,4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(45,0,0);
+    part1->getTransform()->setPosition(1.0,6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    //Pate D
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-75,0,0);
+    part1->getTransform()->setPosition(0.0,-4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(0.0,-4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-45,0,0);
+    part1->getTransform()->setPosition(0.0,-6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    //Pate D
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-75,0,0);
+    part1->getTransform()->setPosition(1.0,-4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(1.0,-4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-45,0,0);
+    part1->getTransform()->setPosition(1.0,-6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    //Pate D
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-75,0,0);
+    part1->getTransform()->setPosition(-1.0,-4.0,2.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(-1.0,-4.0,2.1);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(-45,0,0);
+    part1->getTransform()->setPosition(-1.0,-6.0,0.0);
+    part1->getTransform()->setScale(0.2,0.2,3);
+    addPart(part1);
+    //Antennes
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(0,60,30);
+    part1->getTransform()->setPosition(2.5,0.3,2.0);
+    part1->getTransform()->setScale(0.1,0.1,3);
+    addPart(part1);
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(5.0,0.3,3.5);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
+    
+    part1=new Cylinder();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setOrientation(0,60,-30);
+    part1->getTransform()->setPosition(2.5,-0.3,2.0);
+    part1->getTransform()->setScale(0.1,0.1,3);
+    addPart(part1);
+    part1=new Sphere();
+    part1->getMaterial()->setAmbient(0.0,0.0,0.0);
+    part1->getTransform()->setPosition(5.0,-0.3,3.5);
+    part1->getTransform()->setScale(0.3,0.3,0.3);
+    addPart(part1);
 }
 
 AntShape::~AntShape()
